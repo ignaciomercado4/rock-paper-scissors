4c2e9b40fb847bd35bd6e41c8832cd74cd3a8ab9
Ignacio Mercado <igmercado46@gmail.com>
2024-12-02 02:09:44 -0300

    game works

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ func getUserChoice (options [3]string) string {
 	var userChoice int
 	var finalUserChoice string
 
-	fmt.Println("Select an option: 0 for ROCK - 1 for PAPER - 2 for SCISSORS")
+	fmt.Println("Select an option: \n+ 0 for Rock \n+ 1 for Paper \n+ 2 for Scissors")
 	fmt.Scan(&userChoice)
 
 	for i:= range 3 {
@@ -28,7 +28,63 @@ func getUserChoice (options [3]string) string {
 	return finalUserChoice
 }
 
+func getGameResult(userChoice string, machineChoice string) string {
+	var result string = "UNKNOWN"
+	
+	if userChoice != "" {
+		switch userChoice {
+		case "Rock":
+			if machineChoice == "Rock" {
+				result = "TIE"
+			}
+
+			if machineChoice == "Paper" {
+				result = "LOSE"
+			} else {
+				result = "WIN"
+			}
+
+		case "Paper":
+			if machineChoice == "Rock" {
+				result = "WIN"
+			}
+
+			if machineChoice == "Paper" {
+				result = "TIE"
+			} else {
+				result = "LOSE"
+			}
+
+		case "Scissors": 
+			if machineChoice == "Rock" {
+				result = "LOSE"
+			}
+
+			if machineChoice == "PAPER" {
+				result = "WIN"
+			} else {
+				result = "TIE"
+			}
+
+		default:
+			result = "UNKNOWN"
+		}
+	}
+
+	return result
+}
+
 func main() {
 	options := [3]string{"Rock", "Paper", "Scissors"}
-	fmt.Println(getUserChoice(options))
+	
+	userChoice := getUserChoice(options)
+	machineChoice := getMachineChoice(options)
+	result := getGameResult(userChoice, machineChoice)
+
+	if result == "UNKNOWN" {
+		fmt.Println("INVALID INPUT")
+	} else {
+		fmt.Println("MACHINE CHOSE", machineChoice, "\n YOU", result)
+	}
+	
 }
